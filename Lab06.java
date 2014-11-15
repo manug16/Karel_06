fe9f1f0467b7464b38c98ee4c6f38e0e3f538289
manug16 <manu@ManuMacBook.local>
2014-11-15 20:29:31 +0000

    Lab06 + Extra Exercises Manu Gualandri
    Ready to hand in

--- Lab06.java
+++ Lab06.java
@@ -1,17 +1,64 @@
 /**
 *
-* Description of the program goes here  // provide a brief description
+*   // 6 Robots perform 6 tasks
 *
-* @author <Your Name here>  // replace <...> with your name
-* @version <date/of/completion> // replace <...> with the date
+* @author <Manu Gualandri>  // replace <...> with your name
+* @version <12/11/2014> // replace <...> with the date
 */
 
 import edu.fcps.karel2.Display;
 import edu.fcps.karel2.Robot;
+import javax.swing.JOptionPane;
 
 public class Lab06 {
 
      public static void main(String[] args) {
-          // TODO Your code goes here
+     String filename = JOptionPane.showInputDialog("What robot map?");  	
+     Display.openWorld("maps/" + filename + ".map");
+     Display.setSize(10, 10);
+     Display.setSpeed(8);
+     Robot manu1 = new Robot();
+     Robot manu2 = new Robot(1, 2, Display.EAST, Display.INFINITY);
+     Robot manu3 = new Robot(1, 3, Display.EAST, Display.INFINITY);
+     Robot manu4 = new Robot(1, 4, Display.EAST, Display.INFINITY);
+     Robot manu5 = new Robot(1, 5, Display.EAST, Display.INFINITY);
+     Robot manu6 = new Robot(1, 6, Display.EAST, Display.INFINITY); 
+     
+     while(manu1.nextToABeeper()) {
+         manu1.move();
      }
+     while(!manu2.nextToABeeper()) {
+         manu2.move();
+     }
+     while(manu3.frontIsClear()) {
+         manu3.move();
+     }
+     while(manu4.frontIsClear()) {       
+         while(manu4.nextToABeeper()) {
+             manu4.pickBeeper();
+         }
+         manu4.move();
+     } 
+     if(!manu4.frontIsClear() && manu4.nextToABeeper()) {
+         manu4.pickBeeper();
+     }
+     while(manu5.frontIsClear()) {       
+         while(manu5.nextToABeeper()) {
+             manu5.pickBeeper();
+         }
+         manu5.move();
+     }
+     while(!manu5.frontIsClear() && manu5.nextToABeeper()) {  
+         manu5.pickBeeper();
+     } 
+     while(manu6.nextToABeeper()) {  
+         manu6.move();
+     }
+     if(manu6.frontIsClear()) {
+         manu6.move();
+         while(manu6.nextToABeeper()) {
+            manu6.move();
+         }
+     }    
+}
 }
---
+++ Lab06Extension.java
@@ -0,0 +1,63 @@
+import edu.fcps.karel2.Display;
+import edu.fcps.karel2.Robot;
+
+public class Lab06Extension {
+
+     public static void dropBeepersAndMove (Robot arg) {
+     int beepers = 4;
+     while(beepers > 0) {		
+     arg.putBeeper();		
+     beepers--;
+     }
+     arg.move();
+     }
+     
+     public static void main(String[] args) {  	
+     Display.openWorld("maps/CustomLab06.map");
+     Display.setSize(11, 11);
+     Display.setSpeed(8);
+     Robot manu7 = new Robot();
+     Robot manu8 = new Robot(1, 2, Display.EAST, 10);
+     Robot manu8task = new Robot(5, 2, Display.EAST, Display.INFINITY);
+     Robot manu9 = new Robot(1, 3, Display.EAST, Display.INFINITY);
+     Robot manu10 = new Robot(1, 4, Display.EAST, Display.INFINITY);
+     Robot manu11 = new Robot(1, 5, Display.EAST, Display.INFINITY);
+     Robot manu12 = new Robot(1, 6, Display.EAST, Display.INFINITY); 
+     
+     int count = 0;
+     while(manu7.frontIsClear ()) {		
+     manu7.move();		
+     count++;
+     }
+     System.out.println("Number of steps is " + count + ".");
+     
+	  while(!manu8.nextToARobot ()) {		
+     manu8.move();	
+     }
+     while(manu8.hasBeepers ()) {		
+     manu8.putBeeper();		
+     } 
+     
+     dropBeepersAndMove(manu9);
+     dropBeepersAndMove(manu9);
+     dropBeepersAndMove(manu9);
+     dropBeepersAndMove(manu9);
+     dropBeepersAndMove(manu9);
+     
+     while(manu10.frontIsClear()) {
+         while(manu10.nextToABeeper()) {		
+         manu10.move();	
+         }				    
+         manu10.putBeeper();
+     }
+
+     while(!manu11.rightIsClear()) {		
+         manu11.putBeeper();
+         manu11.move();
+     }
+  
+     while(manu12.rightIsClear() || !manu12.nextToABeeper()) {	
+     manu12.move();
+     }
+}
+}
